43819dc70d13d50c7c001c8ebafc17706e96cea4
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-05-13 17:16:47 +0500

    Fixed Delivery

--- src/pages/Delivery/Delivery.jsx
+++ src/pages/Delivery/Delivery.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Delivery = () => {
-    return (
-        <div>
-            Delivery
-        </div>
-    );
-}
+  return (
+    <div className="flex justify-between px-36">
+      <div className="">one</div>
+      <div className="">Two</div>
+      <div className="">Three</div>
+      <div className="">Three</div>
+      <div className="">Three</div>
+    </div>
+  );
+};
 
 export default Delivery;
