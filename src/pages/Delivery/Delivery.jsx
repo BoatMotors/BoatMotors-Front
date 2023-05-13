7ad084133546888db95121b28eb60b8cec4f6a8e
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-05-13 22:57:50 +0500

    Own Delivery Section Done!

--- src/pages/Delivery/Delivery.jsx
+++ src/pages/Delivery/Delivery.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DeliveryPayment from "../../components/Delivery/DeliveryPayment";
 import FreeDelivery from "../../components/Delivery/FreeDelivery";
+import OwnDeliveryService from "../../components/Delivery/OwnDeliveryService";
 
 const Delivery = () => {
   return (
     <div className="px-[140px]">
       <DeliveryPayment />
       <FreeDelivery />
+      <OwnDeliveryService />
     </div>
   );
 };
