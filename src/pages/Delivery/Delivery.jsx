69e8bc629c8df566006c352eb305d8474992a5ed
Uraimoff <uraimovibrohim5@gmail.com>
2023-05-24 19:13:21 +0500

    pull

--- src/pages/Delivery/Delivery.jsx
+++ src/pages/Delivery/Delivery.jsx
@@ -5,19 +5,20 @@ import OwnDeliveryService from "../../components/Delivery/OwnDeliveryService";
 import PaymentForGoods from "../../components/Delivery/PaymentForGoods";
 import PaymentRequizit from "../../components/Delivery/PaymentRequizit";
 import ReviewsForm from "../../components/Reviews/ReviewsForm";
+import Header from './../../components/Header/Header'
 
 const Delivery = () => {
   return (
     <>
     <Header BigTxt={'MOTORSBOAT'} SubTxt={'лодки & моторы'} fontStyle={'nordinRegular'} />
     <div className="px-[140px]">
-      <DeliveryPayment />
-      <FreeDelivery />
-      <OwnDeliveryService />
-      <DeliverySDEK />
-      <PaymentForGoods />
-      <PaymentRequizit />
-      <ReviewsForm />
+      <DeliveryPayment/>
+      <FreeDelivery/>
+      <OwnDeliveryService/>
+      <DeliverySDEK/>
+      <PaymentForGoods/>
+      <PaymentRequizit/>
+      <ReviewsForm/>
     </div>
   </>
   );
